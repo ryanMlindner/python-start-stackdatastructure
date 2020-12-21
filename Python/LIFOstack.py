ea77ffe593b99072bedd9b4f44afa42ef4279876
Ryan Lindner <ryan.m.lindner@gmail.com>
2020-12-21 13:25:28 -0700

    main change to lifoStack to pass vars instead of printing
    updated stack class to pass variables for cleaner code and easier testing, current problems exist in test file, am working on them, this is an interim push

--- Python/LIFOstack.py
+++ Python/LIFOstack.py
@@ -3,60 +3,65 @@ class stack:
    def __init__(self):
       self.storage = list()
 
-## TODO make functs return vars
+## TODOne make functs return vars
 
    def peek(self):
+      var = 0
       if self.storage:
-         print("top item: \"" + self.storage[-1] + "\"")
-      else:
-         print("cannot peek, stack is empty")
+         var = self.storage[-1]
+      return var
 
    def pop(self):
+      var = 0
       if self.storage:
-         print("top item removed: \"" + self.storage[-1] + "\"")
+         var = self.storage[-1]
          self.storage.pop()
-      else: print("cannot pop, stack is empty")
+      return var
 
    def push(self, input):
       self.storage.append(input)
-      print("input: \"" + input + "\" successfully added")
+      return input
 
-   def isEmpty(self):
-      print("Not Empty") if self.storage else print("Empty")
-
-done = 0
-dataStack = stack()
-
-## TODO make functions in stack return variables
+## TODOne make functions in stack return variables
 ## to make them testable easier
+if __name__ == '__main__' :
+   done = 0
+   dataStack = stack()
 
-while not(bool(done)):
-   userData = input("use pop, peek, isEmpty, exit, or use push for data input:")
+   while not(bool(done)):
+      userData = input("use pop, peek, isEmpty, exit, or use push for data input:")
    
    ## while loop for data entry, can be entered and exited at will
    ## by the user
-   if userData == "push":
+      if userData == "push":
 
       ## flag var for finishing while loop
-      entryDone = 0
+         entryDone = 0
 
-      while not(bool(entryDone)):
-         userData2 = input("enter datum for entry, or \'*\' to go back:")
-         if userData2 == "*":
-            entryDone = 1
-         else:
-            dataStack.push(userData2)
+         while not(bool(entryDone)):
+            userData2 = input("enter datum for entry, or \'*\' to go back:")
+            if userData2 == "*":
+               entryDone = 1
+            else:
+               print("input: \"" + dataStack.push(userData2)
+               + "\" successfully added")
 
-   elif userData == "isEmpty":
-      dataStack.isEmpty()
+      elif userData == "isEmpty":
+         if dataStack.peek(): print("Not Empty")
+         else: print("Empty")
+         
 
-   elif userData == "pop":
-      dataStack.pop()
+      elif userData == "pop":
+         if dataStack.peek():
+            print("top item: \"" + dataStack.pop() + "\" removed")
+         else: print("cannot pop, stack is empty")
 
-   elif userData == "peek":
-      dataStack.peek()
+      elif userData == "peek":
+         if dataStack.peek():
+            print("top item: \"" + dataStack.peek() + "\"")
+         else: print("cannot peek, stack is empty")
 
-   elif userData == "exit":
-      done = 1  
+      elif userData == "exit":
+         done = 1  
 
 exit()
---
+++ Python/lifo_test.py
@@ -0,0 +1,29 @@
+import lifoStack
+import unittest
+##TODO answer why 'import lifoStack' does not work, pushing
+##code for now for transparency and how
+##i track this problem for the future
+class testStack(unittest.TestCase):
+    def setUp(self):
+        self.stack = lifoStack.stack()
+        self.stack.push("data1")
+        self.stack.push("data2")
+    def test_stack_empty(self):
+        ## stack is empty
+        self.assertTrue(self.stack.peek())
+    def test_peek_works(self):
+        ## comparing top value to expected
+        self.assertEqual(self.stack.peek(), "data2", "success")
+    def test_pop_works(self):
+        ## popping data2 and seeing data1 on top
+        self.stack.pop()
+        self.assertEqual(self.stack.peek(), "data1")
+    def tearDown(self):
+        while self.stack.storage:
+            self.stack.pop()
+        self.assertFalse(self.stack.storage)
+
+if __name__ == '__main__' :
+    print("running")
+    unittest.main()
+    print("ran")
